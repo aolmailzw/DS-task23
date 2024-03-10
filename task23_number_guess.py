52e32bc06c5f723cea5ef72c401a3ed5927c9975
aolmailzw <162926651+aolmailzw@users.noreply.github.com>
2024-03-10 22:34:56 +0000

    Improve codes readability with comments and convert user input to int

--- task23_number_guess.py
+++ task23_number_guess.py
@@ -1,18 +1,23 @@
-# Number Guess Game
+# Improved Number Guess Game with:
+# 1> Adding int() function to convert input to int 
+# 2> Adding comments to the code
 
-target = 10  
-attempts = 0  
+target = 10  # The number to guess
+attempts = 0  # Counting attempts
 
 while attempts < 3:  # Allow up to 3 guesses
     user_input = input("Please input your guess: ")
+    guess = int(user_input)  # Convert input to integer
     
-    if user_input > target:
+    if guess > target:
         print("Your guess is too large.")
-    elif user_input < target:
+    elif guess < target:
         print("Your guess is too small.")
     else:
         print("Congratulations! You've guessed it right.")
-        break  
-    attempts += 1  
+        break  # Exit the loop if guessed correctly
+    
+    attempts += 1  # Increment the attempt counter
+
 if attempts == 3:
     print("3 attempts finished, you have no more chances.")
